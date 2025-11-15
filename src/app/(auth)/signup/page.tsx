a0f9ecbc8b548f7125da8f08df7fbaa121289b5a
praveen.jeeru <praveenjeeru68@gmail.com>
2025-11-15 20:39:34 +0530

    update commit

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,10 @@
+'use client';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { toast } from '@/hooks/use-toast';
 
 export default function SignupPage() {
   return (
@@ -26,7 +28,13 @@ export default function SignupPage() {
         </div>
       </CardContent>
       <CardFooter className="flex flex-col gap-4">
-        <Button className="w-full">Create Account</Button>
+        <Button className="w-full" onClick={()=>{
+          toast({
+        variant: 'destructive',
+        title: 'Invalid Credentials',
+        description: 'Please check your email and password.',
+      });
+        }}>Create Account</Button>
         <div className="text-center text-sm">
           Already have an account?{' '}
           <Link href="/login" className="underline">
